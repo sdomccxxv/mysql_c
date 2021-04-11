343e1b2c57d025d54a8e2097e90cc2e1a497566f
Samuel Ortiz <samuel.om@alliedglobalbpo.com>
2021-04-10 23:39:42 -0600

    Se agrega la funcion mostrar y el menu.

--- mysql_c.cpp
+++ mysql_c.cpp
@@ -7,6 +7,11 @@ void menu();
 
 int main()
 {
+    menu();
+
+}
+
+void Ingresar() {
     MYSQL* conectar;
     conectar = mysql_init(0);
     conectar = mysql_real_connect(conectar, "localhost", "root", "150289", "db_empresa1", 3306, NULL, 0);
@@ -32,7 +37,49 @@ int main()
     }
     //cout << "Hello World!\n";
     system("pause");
-    return 0;
-    
+    //return 0;
+}
+
+void Mostrar() {
+    MYSQL* conectar;
+    MYSQL_ROW fila;
+    MYSQL_RES* resultado;
+
+    conectar = mysql_init(0);
+    conectar = mysql_real_connect(conectar, "localhost", "root", "150289", "db_empresa1", 3306, NULL, 0);
+    if (conectar) {
+        string consulta = "SELECT * FROM puestos";
+        const char* c = consulta.c_str();
+        q_estado = mysql_query(conectar, c);
+
+        if (!q_estado) {
+            resultado = mysql_store_result(conectar);
+            while (fila = mysql_fetch_row(resultado)) {
+                cout << fila[0] << " - " << fila[1] << endl;
+            }
+        }
+        else {
+            cout << "Error al consultar..." << endl;
+        }
+
+    }
+    else {
+        cout << "Conexion fallida..." << endl;
+    }
+    system("pause");
+}
+
+void menu() {
+
+    int puesto;
+    cout << "Si desea ingresar un nuevo puesto presione 1, si desea ver los puestos presione 2: ";
+    cin >> puesto;
+    switch (puesto)
+    {
+    case 1: Ingresar();
+    case 2: Mostrar();
+    default:
+        break;
+    }
 }
 
